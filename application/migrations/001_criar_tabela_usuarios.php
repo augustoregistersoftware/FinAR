86b12fa079fdbcb4f08b378d96da8cae02484fca
augusto valenciano <augusto.valenciano@fabricadecodigos.com.br>
2024-04-08 10:58:41 -0300

    SPRINT adicionado migrations para estudar

---
+++ application/migrations/001_criar_tabela_usuarios.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Criar_tabela_usuarios extends CI_Migration {
+
+    public function up() {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
+            ),
+            'name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ),
+            'description' => array(
+                'type' => 'TEXT',
+                'null' => TRUE,
+            ),
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('products');
+    }
+
+    public function down() {
+        $this->dbforge->drop_table('products');
+    }
+}
